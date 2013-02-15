2ec04d6325005808df7f11cbcaa68b0909817bb7
Yaniv Aknin <yaniv@aknin.name>
2013-02-15 11:31:10 +0200

    devops: enrich configuration elements, add SERVER_NAME

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,16 @@
-from os import environ as CONFIG
+from os import environ
 
-DEBUG=bool(int(CONFIG['DEBUG']))
+from utils.ext.path import Path
+from urlobject import URLObject
+
+required = lambda s: environ[s]
+optional = lambda s, d=None: environ.get(s, d)
+boolean = lambda v: bool(int(v))
+netloc = lambda n: URLObject().with_netloc(n)
+
+DEBUG=boolean(required('DEBUG'))
+VAR_DIR=Path(required('VAR_DIR'))
+SERVER_NAME=required("SERVER_NAME")
+SERVER_URL=netloc(SERVER_NAME)
+
+del required, optional, boolean
--- runcommands.sh
+++ runcommands.sh
@@ -1,8 +1,15 @@
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 root="$( cd "$( dirname "$0" )" && pwd )"
 
+# configuration
 export DEBUG=1
+export SERVER_NAME='localhost:5000'
 
+# local runcommands
 [ -f "${root}"/runcommands.local.sh ] && source runcommands.local.sh
+
+# provision directories for dev environment
+export VAR_DIR="${root}/.venv/var"
+mkdir -p $VAR_DIR
 
 unset root
