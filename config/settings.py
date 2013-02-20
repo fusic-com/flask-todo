3dba3b57e6a036d64cdbe675f08630941b12b0fc
Yaniv Aknin <yaniv@aknin.name>
2013-02-20 19:37:18 +0200

    devops: add explicit configuration validation

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,30 @@
 from os import environ
 
 from utils.ext.path import Path
-from urlobject import URLObject as Url
+try:
+    from urlobject import URLObject as Url
+except ImportError:
+    if 'JJ_VALIDATE_CONFIG' not in environ:
+        raise
+    class Url(object):
+        __init__ = lambda s, *a: None
+        __getattr__ = __call__ = lambda s, *a: s
 
 required = lambda s: environ[s]
 optional = lambda s, d=None: environ.get(s, d)
 boolean = lambda v: bool(int(v))
 netloc = lambda n: Url().with_netloc(n)
 
-DEBUG=boolean(required('DEBUG'))
-VAR_DIR=Path(required('VAR_DIR'))
-SERVER_NAME=required("SERVER_NAME")
-SERVER_URL=netloc(SERVER_NAME)
-AWS_ACCESS_KEY_ID=optional('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY=optional('AWS_SECRET_ACCESS_KEY')
-SQLALCHEMY_DATABASE_URI = Url(required('DATABASE_URL'))
-SECRET_KEY=required('SECRET_KEY')
+try:
+    DEBUG=boolean(required('DEBUG'))
+    VAR_DIR=Path(required('VAR_DIR'))
+    SERVER_NAME=required("SERVER_NAME")
+    SERVER_URL=netloc(SERVER_NAME)
+    AWS_ACCESS_KEY_ID=optional('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY=optional('AWS_SECRET_ACCESS_KEY')
+    SQLALCHEMY_DATABASE_URI = Url(required('DATABASE_URL'))
+    SECRET_KEY=required('SECRET_KEY')
+except KeyError, key:
+    raise SystemExit("environment missing required key %s" % (key,))
 
 del required, optional, boolean
